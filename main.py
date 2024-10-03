b269efcabaed43b16510a210f661334149c48e7d
ghaisan <ghaisanrabbani5@gmail.com>
2024-10-03 18:20:21 +0700

    Add Payment Validation

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-harga_apel=10000
-harga_jeruk=15000
-harga_anggur=20000
+price_apple=10000
+price_orange=15000
+price_grape=20000
 
-jumlah_apel=int(input("masukkan jumlah apel="))
-jumlah_jeruk=int(input("masukkan jumlah jeruk="))
-jumlah_anggur=int(input("masukkan jumlah anggur="))
+amount_apple=int(input("masukkan jumlah apel="))
+amount_orange=int(input("masukkan jumlah jeruk="))
+amount_grape=int(input("masukkan jumlah anggur="))
 
-total_apel=harga_apel*jumlah_apel
-total_jeruk=harga_jeruk*jumlah_jeruk
-total_anggur=harga_anggur*jumlah_anggur
-total=total_apel+total_jeruk+total_anggur
+total_apple=price_apple*amount_apple
+total_orange=price_orange*amount_orange
+total_grape=price_grape*amount_grape
+total=total_apple+total_orange+total_grape
 
-print("===detail Belanja===")
-print("apel: ",jumlah_apel," x ",harga_apel," = ",total_apel)
-print("jeruk: ",jumlah_jeruk," x ",harga_jeruk," = ",total_jeruk)
-print("anggur: ",jumlah_anggur," x ",harga_anggur," = ",total_anggur)
+print("\n===detail Belanja===")
+print(f"apel: {amount_apple} x {price_apple} = {total_apple}")
+print(f"jeruk: {amount_orange} x {price_orange} = {total_orange}")
+print(f"grape: {amount_grape} x {price_grape} = {total_grape}")
 
-print("total belanja: ",total)
+print("\ntotal belanja: ",total)
+pay=int(input("\nmasukkan jumlah uang:"))
+
+if total>pay:
+    mines_money=total-pay
+    print("transaksi anda dibatalkan")
+    print(f"uang kurang sebesar {mines_money}\n\n")
+elif total==pay:
+    print("terimakasih\n\n")
+else:
+    surplus_money=pay-total
+    print("terimakasih")
+    print(f"\nuang kembali anda: {surplus_money}\n\n")
